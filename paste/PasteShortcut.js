f8c1b0c8608abeb801a11c4a82adaa367a4b7c00
sahasd <sahasd@gmail.com>
2016-03-24 07:33:53 -0700

    Fixed problem where data isnt being sent to pastebin

--- paste/PasteShortcut.js
+++ paste/PasteShortcut.js
@@ -1,19 +1,30 @@
 var fs = require("fs");
-var PastebinAPI = require("pastebin-js");
+var paste = require("better-pastebin");
 var binUser = JSON.parse(fs.readFileSync(__dirname+"/credentials.json"));
-    pastebin = new PastebinAPI({
-      'api_dev_key' : binUser.key,
-      'api_user_name' : binUser.user,
-      'api_user_password' : binUser.pass
+paste.setDevKey(binUser.key);
+paste.login(binUser.user, binUser.pass, function(success, data) {
+    if(!success) {
+        console.log("Failed (" + data + ")");
+        return false;
+    }
+    paste.create({
+        contents: fs.readFileSync(__dirname + "/pasted.txt") ,
+        name: "test pasting",
+        format: "javascript"
+    }, function(success, data) {
+        if(success) {
+          console.log(data);
+          fs.writeFile(__dirname+"/pasted.txt", data, function (err){
+            if (err) {
+              return console.log(err);
+            }
+          });
+
+        }
     });
-pastebin.createPasteFromFile(__dirname+"/pasted.txt", "pastebin-js test", null, 1, "N")
-    .then(function (data) {
-        fs.writeFile(__dirname+"/pasted.txt", "pastebin.com/" + data, function (err){
-			if (err) {
-			return console.log(err);
-			}
-			console.log("pastebin.com/" + data);
-		});
-    });
+});
+
+
+
 
 
