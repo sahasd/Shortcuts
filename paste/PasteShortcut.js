ae29998430cf02fdcb987f7ecace837403fe948d
sahasd <sahasd@gmail.com>
2016-03-23 22:17:28 -0700

    fixed bug of not recognizing files when service is run

--- paste/PasteShortcut.js
+++ paste/PasteShortcut.js
@@ -1,14 +1,14 @@
 var fs = require("fs");
-var PastebinAPI = require('pastebin-js');
-var binUser = JSON.parse(fs.readFileSync("credentials.json"));
+var PastebinAPI = require("pastebin-js");
+var binUser = JSON.parse(fs.readFileSync(__dirname+"/credentials.json"));
     pastebin = new PastebinAPI({
       'api_dev_key' : binUser.key,
       'api_user_name' : binUser.user,
       'api_user_password' : binUser.pass
     });
-pastebin.createPasteFromFile("pasted.txt", "pastebin-js test", null, 1, "N")
+pastebin.createPasteFromFile(__dirname+"/pasted.txt", "pastebin-js test", null, 1, "N")
     .then(function (data) {
-        fs.writeFile("pasted.txt", "pastebin.com/" + data, function (err){
+        fs.writeFile(__dirname+"/pasted.txt", "pastebin.com/" + data, function (err){
 			if (err) {
 			return console.log(err);
 			}
