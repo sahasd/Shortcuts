43c007e5d1b8f454ddce9272dad716423b27f483
sahasd <sahasd@gmail.com>
2016-03-23 22:03:29 -0700

    Finished paste shortcut

--- paste/PasteShortcut.js
+++ paste/PasteShortcut.js
@@ -1,7 +1,19 @@
 var fs = require("fs");
-fs.writeFile("paste.txt", "google.com", function (err){
-	if (err) {
-		return console.log(err);
-	}
-	console.log("The file was completed");
-});
+var PastebinAPI = require('pastebin-js');
+var binUser = JSON.parse(fs.readFileSync("credentials.json"));
+    pastebin = new PastebinAPI({
+      'api_dev_key' : binUser.key,
+      'api_user_name' : binUser.user,
+      'api_user_password' : binUser.pass
+    });
+pastebin.createPasteFromFile("pasted.txt", "pastebin-js test", null, 1, "N")
+    .then(function (data) {
+        fs.writeFile("pasted.txt", "pastebin.com/" + data, function (err){
+			if (err) {
+			return console.log(err);
+			}
+			console.log("pastebin.com/" + data);
+		});
+    });
+
+
